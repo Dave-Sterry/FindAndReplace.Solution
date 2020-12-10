e957b79e62f020070e0c5e159a896d5c7c8b6d7c
David Sterry <sterry.david@gmail.com>
2020-12-10 11:54:56 -0800

    pass third test
    Co-authored-by: Eric Endsley <eric.endsley4@gmail.com>

--- FindAndReplace/Models/Find.cs
+++ FindAndReplace/Models/Find.cs
@@ -1,9 +1,39 @@
+using System;
+using System.Collections.Generic;
+
 namespace FindAndReplace.Models
 {
-  public class Item
+  public class Find
   {
 
-    // properties, methods, etc. will go here.
-
+    public static string Replace(string userPhrase, string wordToReplace, string replaceWith) 
+    { 
+      if (userPhrase.Contains(wordToReplace))
+      {
+        string returnString = ""; 
+        string[] phraseArr = userPhrase.Split(" ");
+        for (int i = 0; i < 1; i++) 
+        {
+          if (phraseArr[i] == wordToReplace)
+          {
+            phraseArr[i] = replaceWith;
+            returnString = string.Join(" ", phraseArr);
+          }
+          else 
+          {
+           //do nothing
+          }
+        } 
+        return returnString;
+      }
+      if (userPhrase == wordToReplace)
+      {
+        return replaceWith;
+      }
+      else
+      {
+        return userPhrase;
+      }
+    } 
   }
 }
