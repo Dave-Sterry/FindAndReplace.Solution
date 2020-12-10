9500bb64a3af83d6746b9832fd3bb1bc19a3f711
David Sterry <sterry.david@gmail.com>
2020-12-10 10:50:12 -0800

    purposefully pass first test
    Co-authored-by: Eric Endsley <eric.endsley4@gmail.com>

--- FindAndReplace.Test/ModelTests/FindTest.cs
+++ FindAndReplace.Test/ModelTests/FindTest.cs
@@ -6,7 +6,13 @@ namespace FindTest.Tests
   [TestClass]
   public class FindTest
   {
-
+    
+    [TestMethod]
+    public void Replace_ReplaceExactMatch_String()
+    {
+      string result = Find.Replace("ab", "bc", "cd");
+      Assert.AreEqual("false", result);
+    }
     // Test methods will go here.
 
   }
