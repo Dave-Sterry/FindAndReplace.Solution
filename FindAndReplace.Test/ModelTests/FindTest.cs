7e9f8683f4ff08ad883de42b5039a6ef7600f19c
David Sterry <sterry.david@gmail.com>
2020-12-10 10:31:30 -0800

    initialized project
    Co-authored-by: Eric Endsley <eric.endsley4@gmail.com>

---
+++ FindAndReplace.Test/ModelTests/FindTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FindAndReplace.Models;
+
+namespace FindTest.Tests
+{
+  [TestClass]
+  public class FindTest
+  {
+
+    // Test methods will go here.
+
+  }
+}
